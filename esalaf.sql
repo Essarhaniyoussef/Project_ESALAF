6f5fa209f1ffa5243eb5d98eaab0c5d7e2db8417
Youssef_Essarhani <121612580+Essarhaniyoussef@users.noreply.github.com>
2023-08-23 00:20:44 +0100

    first commit

---
+++ esalaf.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 02 avr. 2023 à 21:50
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE esalaf;
+USE esalaf;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `esalaf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(100) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `cin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom`, `telephone`, `cin`) VALUES
+(1, 'Ayman', '0683907376', 'K573926'),
+(2, 'Ahmed', '0393827262', 'KB262727'),
+(3, 'Imad', '0937839836', 'K383726'),
+(4, 'Salma', '0933983878', 'K388737'),
+(5, 'Hakim', '0625839127', 'K632598'),
+(6, 'Achraf', '0674916058', 'K124869'),
+(7, 'Youssef', '0621298345', 'K907142'),
+(8, 'Fahd', '0684572903', 'K238406'),
+(9, 'Mohamed', '0637482190', 'K846193'),
+(10, 'Othman', '0673509418', 'K751390'),
+(11, 'Morad', '0630827459', 'K320759'),
+(12, 'Soufian', '0690372581', 'K981647'),
+(13, 'Salim', '0665941280', 'K589324'),
+(14, 'Yassine', '0612349875', 'K743681');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `id` int(100) NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `produit` varchar(255) NOT NULL,
+  `quantite` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `cin`, `produit`, `quantite`, `etat`) VALUES
+(8, 'K743681', 'Hawai', '3', 'Non payé');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id_produit` int(100) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prix` float(30,2) NOT NULL,
+  `quantite` varchar(255) NOT NULL,
+  `categories` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id_produit`, `nom`, `prix`, `quantite`, `categories`) VALUES
+(1, 'Hawai', 13.95, '10', 'BOISSONS'),
+(4, 'Coca Cola', 17.95, '19', 'BOISSONS');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id_produit`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id_produit` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
